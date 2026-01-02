420edb0030b3f233134ec35c20cbda751a688f37
brainito21 <brainito21@gmail.com>
2026-01-02 18:19:43 +0530

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,15 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ContactSerializer
+
+class ContactCreateView(APIView):
+    def post(self, request):
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"success": True}, status=status.HTTP_201_CREATED)
+        return Response(
+            {"success": False, "errors": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
